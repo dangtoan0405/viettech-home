59bfa258a436788aa9018f229511bc3c258a8a00
Toan Dang <dangtoan0405@gmail.com>
2020-09-10 17:54:56 +0700

    update erp item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import * as AOS from 'aos';
+import { HttpClient } from '@angular/common/http';
+import { ItemERP } from 'src/model/item-erp';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,8 +11,13 @@ import * as AOS from 'aos';
 export class AppComponent implements OnInit {
   title = 'home-viettech';
   itemSelected = '1';
-  constructor(private translateService: TranslateService) {
+  itemERP: ItemERP[] = [];
+  constructor(
+    private translateService: TranslateService,
+    private http: HttpClient
+  ) {
     this.translateService.setDefaultLang('vn');
+    this.getDataJSONFile();
   }
 
   public selectItem(id: string) {
@@ -24,8 +31,34 @@ export class AppComponent implements OnInit {
     });
   }
 
+  getDataJSONFile() {
+    this.http
+      .get('assets/data.json')
+      .subscribe((res: { itemERP: ItemERP[] }) => {
+        this.itemERP = res.itemERP;
+      });
+  }
+
   gotoTop() {
     // const el = document.getElementById('targetView');
     // el.scrollIntoView();
+  }
+
+  collapseDescription(elementId) {
+    const el = document.getElementById(
+      elementId + '-description'
+    ) as HTMLElement;
+    const elParent = document.getElementById(
+      elementId + '-description-title'
+    ) as HTMLElement;
+    if (el) {
+      if (el.style.display === 'block' || !el.style.display) {
+        el.style.display = 'none';
+        elParent.classList.add('active-down');
+      } else {
+        el.style.display = 'block';
+        elParent.classList.remove('active-down');
+      }
+    }
   }
 }
--- src/model/item-erp.ts
+++ src/model/item-erp.ts
@@ -1,6 +1,7 @@
 export interface ItemERP {
-  id: string;
-  title: string;
-  img: string;
-  description: string;
+  id?: string;
+  title?: string;
+  description?: string;
+  detail?: ItemERP[];
+  float?: string;
 }
